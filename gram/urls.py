8a992d03e409176e42d97f554bbc113ae110dc2a
lorderonnie <rontheking45@gmail.com>
2020-01-06 11:57:20 +0300

    created forms.py

--- gram/urls.py
+++ gram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('instagram.urls')),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.http  import HttpResponse,Http404
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 from .models import Accounts
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import logout
 
 def agram(request):
     accounts=Accounts.all_accounts()
@@ -22,9 +26,26 @@ def agram(request):
 #         return render(request, 'insta/search.html',{"message":message})
  
 
+@login_required(login_url="/accounts/login/")
+def logout(request):
+  
+  logout(request)
+  return redirect('home')
 
 
+@login_required(login_url = '/accounts/login/')
+def profile(request):
+    my_posts = Image.user_pics(request.user)
+    return render(request,'profile.html',{'my_posts':my_posts})
 
+@login_required(login_url = '/accounts/login/')
+def edit_profile(request):
+    if request.method=='POST':
+        form = EditProfileForm(request.POST,request.FILES,instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
 
-
-
+        else:
+            form = EditProfileForm(instance=request.user)
+        return render(request,'profile.html',{'form':form})
